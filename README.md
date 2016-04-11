d8dc15ca0c0c490403d4b92260c02540598de9ac
James Conkling <james.lane.conkling@gmail.com>
2016-04-10 20:09:42 -0400

    init repo

---
+++ README.md
@@ -0,0 +1,6 @@
+## Installation
+
+```bash
+npm install -g gulp
+npm install
+```
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    source = require('vinyl-source-stream'),
+    browserify = require('browserify'),
+    watchify = require('watchify'),
+    browserSync = require('browser-sync').create();
+
+function bundle (bundler) {
+  return bundler
+    .bundle()
+    .on('error', function(e) {
+      gutil.log(e);
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./app/js/dist'))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('watch', function() {
+  var watcher = watchify(browserify('./app/js/app.js', watchify.args));
+  bundle(watcher);
+  watcher.on('update', function() {
+    bundle(watcher);
+  });
+  watcher.on('log', gutil.log);
+
+  browserSync.init({
+    server: "./app",
+    logFileChanges: false
+  });
+});
+
+gulp.task('js', function() {
+  return bundle(browserify('./app/js/app.js'));
+});
