3825ff458c2887dd342d8ff5abce3dde0f94450b
James Conkling <james.lane.conkling@gmail.com>
2016-06-08 11:42:19 -0400

    build graph of entities

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,9 +6,10 @@
 import {rdfaSubjects2jsonGraph}  from './rdf/rdf-utils';
 const d3 = require('d3');
 const $ = require('jquery');
-const log = console.log.bind(console);
+
 const flatMap = [(flatMap, item) => flatMap.concat(item), []];
 const unique = [(uniqueMap, item) => uniqueMap.indexOf(item) === -1 ? uniqueMap.concat(item) : uniqueMap, []];
+const hashMap = (obj, fn=(val) => val) => Object.keys(obj).map(key => fn(obj[key], key));
 
 const baseURI = 'http://localhost:8080/api/v2/doc/www.npr.org/sections/itsallpolitics/2015/08/20/433253554/iran-lobbying-battle-heats-up-on-the-airwaves/';
 const MARGIN = 30;
@@ -21,18 +22,68 @@ const getLabelsForEntityType = (rdf, entityType) => rdf.getSubjects("fise:entity
 
 const app = {
   init() {
+    this.rdf = document.data;
+    this.getObjectsByCurie = _.curry(getObjectsByCurie)(this.rdf);
+
+
     this.buildGraph();
     this.attachEventListeners();
-    // this.makeD3Graph(this.graph);
+    this.makeD3Graph(this.graph);
   },
 
   buildGraph() {
-    this.rdf = document.data;
 
-    this.getObjectsByCurie = _.curry(getObjectsByCurie)(this.rdf);
-    // const subjects = rdf.getSubjects()
-    //   .map(subjectId => rdf.getSubject(subjectId).toObject());
-    // this.graph = rdfaSubjects2jsonGraph(subjects).graph;
+    const getSubject = this.rdf.getSubject.bind(this.rdf);
+    const expandCurie = this.rdf.expand.bind(this.rdf);
+    const entities = this.rdf.getSubjects()
+      .map(s => this.rdf.getSubject(s))
+      .filter(subject => subject.types.indexOf(expandCurie('fise:EntityAnnotation')) > -1);
+
+    const entityNodes = entities
+      .filter(entity => entity.id)
+      .reduce((nodeMap, entity) => {
+        nodeMap[entity.id] = {
+          id: entity.id,
+          label: entity.getValues('fise:entity-label').join(' - '),
+          type: entity.getValues('fise:entity-type').join(' - ')
+        };
+        return nodeMap;
+      }, {});
+
+    const objectNodes = entities
+      .filter(entity => entity.getValues('fise:entity-reference').length > 0)
+      .filter(entity => entity.id)
+      .reduce((nodeMap, entity) => {
+        entity.getValues('fise:entity-reference')
+          .forEach(objectUri => {
+            nodeMap[objectUri] = {type: 'Object', id: objectUri};
+          });
+
+        return nodeMap;
+      });
+
+    const allNodesMap = Object.assign(entityNodes, objectNodes);
+
+    const edges = entities
+      .filter(entity => entity.getValues('fise:entity-reference').length > 0)
+      .reduce((edges, entity) => {
+        const source = entity.id;
+        const target = entity.getValues('fise:entity-reference')[0];
+        if (!allNodesMap[source] || !allNodesMap[target]) {
+          return edges;
+        }
+        return edges.concat({
+          source: source,
+          target: target,
+          type: 'entity-reference'
+        });
+      }, []);
+
+
+    this.graph = {
+      "nodes": hashMap(allNodesMap),
+      "edges": edges
+    }
   },
 
   attachEventListeners() {
@@ -70,7 +121,7 @@ const app = {
     this.highlightEntityAnnotations(textAnnotationsOfSameEntity);
 
     const types = parentEntities
-      .map(this.getObjectsByCurie('fise:entity-type'))
+      .map(subject => [...this.getObjectsByCurie('fise:entity-type', subject), ...this.getObjectsByCurie('dc:type', subject)]) // does querying for dc:type work?
       .reduce(...flatMap)
       .map(curieOrURL => this.rdf.shorten(curieOrURL) || curieOrURL)
       .reduce(...unique);
@@ -130,22 +181,31 @@ const app = {
         .data(graph.edges)
       .enter().append("line")
         .attr("class", "link")
-        // .style("stroke-width", (d) => Math.sqrt(d.value));
+        .style("stroke-width", (d) => Math.sqrt(d.value));
 
     this.node = domGraph.selectAll(".node")
         .data(graph.nodes)
       .enter().append("circle")
         .attr("class", "node")
         .attr("r", 5)
-        // .style("fill", (d) => color(d.group));
+        .style("fill", (d) => color(d.group))
+        .call(this.force.drag);
 
-    log(this.force);
+    this.force.on('tick', () => {
+      this.link.attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+
+      this.node.attr("cx", (d) => d.x)
+          .attr("cy", (d) => d.y);
+    });
   },
 
   getGraphSize() {
     return {
       width: window.innerWidth - (MARGIN * 2),
-      height: window.innerHeight - (MARGIN * 2)
+      height: (window.innerHeight / 2) - (MARGIN * 2)
     }
   }
 };
